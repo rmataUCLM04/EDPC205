de01101b6d17fd8b7726ec3907c1282b67dbd697
pabloMenchen06 <Pablo.Menchen1@alu.uclm.es>
2025-09-25 12:46:55 +0200

    Add files via upload

---
+++ S02 - Introducción/MainPrograma.java
@@ -0,0 +1,27 @@
+import java.io.FileNotFoundException;
+import utils.SequentialFile;
+
+public class MainPrograma{
+
+    public static void main(String[] args) {
+
+        Satelite sat = new Satelite();
+        try{
+            SequentialFile<Satelite> sf = new SequentialFile<Satelite>("weather.csv");
+            sf.skip();
+
+            while (!sf.isEndOfFile()) {
+                
+                sf.read(sat);
+                if (sat.gotMeanMotion() >= 10) {
+                    System.out.println(sat);
+                }
+            }
+            sf.close();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
