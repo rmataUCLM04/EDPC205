c00465e857330697db6cd86e5db533cdd8854456
rmataUCLM04 <146024866+rmataUCLM04@users.noreply.github.com>
2025-10-15 16:19:57 +0000

    S04 15-10-25

---
+++ S04/java/src/paquete/main.java
@@ -0,0 +1,34 @@
+package paquete;
+import java.io.*;
+import java.util.*;
+public class main {
+    //compare to para las colas con prioridad
+    public static void main(String[] args) {
+        
+        Queue<Peticion> partidosPremiumLargos = new PriorityBlockingQueue<Peticion>();
+        Queue<Peticion> partidosPremium = new PriorityBlockingQueue<Peticion>();
+        Queue<Peticion> partidosNoPremiumLargos = new PriorityBlockingQueue<Peticion>();
+        Queue<Peticion> partidosNoPremiumCortos = new PriorityBlockingQueue<Peticion>();
+         
+    }
+
+    public static void procesarFile(){
+        Peticion nPet;
+        try {
+            FicheroSecuencial<Peticion> ft = new FicheroSecuencial<Peticion>("player_requests.csv");
+            ft.skip();
+
+            while (!ft.isEndOfFile()) {
+                
+                ft.read(nPet);     
+            }
+            ft.close();
+            System.out.println(nPet.toString());
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
