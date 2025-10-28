3e2e1ce4e5c096ed1ce0cceb526c4a0a10c6068b
rmataUCLM04 <146024866+rmataUCLM04@users.noreply.github.com>
2025-10-28 21:18:59 +0100

    Create SequentialFileReader interface
    Added SequentialFileReader interface with readData method.

--- S04/codigo/SequentialFileReader.java
+++ S04/codigo/SequentialFileReader.java
@@ -1,6 +1,44 @@
+/*********************************************************************
+*
+* Nombre de la clase: SequentialFileReader
+* Iniciales autores / Grupo prácticas: RMM, HJC, PMS / G05
+* Fecha de creación: 
+* Versión de la clase: 
+* Descripción de la clase:
+*   Interfaz funcional mínima que deben implementar las clases cuyos
+*   objetos se van a rellenar leyendo datos de un fichero secuencial.
+*   Define el método readData, que recibe los campos ya separados.
+*
+**********************************************************************/
 
 public interface SequentialFileReader {
 
+    /*********************************************************************
+    *
+    * Nombre del método: readData
+    *
+    * Autor original: XX
+    *
+    * Descripción del método:
+    *   Rellena el estado interno del objeto a partir de los datos leídos
+    *   de una línea del fichero. La línea debe haberse separado en campos
+    *   (por ejemplo usando split(";")) antes de llamar a este método.
+    *
+    * Argumentos de entrada:
+    *   String[] data -> array con los valores de la línea descompuesta.
+    *
+    * Valor de retorno:
+    *   void. El propio objeto debe actualizar sus atributos internos.
+    *
+    * Ficheros requeridos:
+    *   Se asume que la lectura del fichero y la apertura/cierre del mismo
+    *   la gestiona otra clase (por ejemplo FicheroSecuencial).
+    *
+    * Excepciones comprobadas:
+    *   Depende de la implementación concreta. Por ejemplo, puede lanzar
+    *   NumberFormatException si algún campo numérico no es válido.
+    *
+    *********************************************************************/
     void readData(String[] data);    
     
 }
