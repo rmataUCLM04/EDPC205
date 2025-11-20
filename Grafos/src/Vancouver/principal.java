e505834cc4b1bf35e4a7e44507d7fd81abfee507
rmataUCLM04 <raul.mata@alu.uclm.es>
2025-11-20 11:38:35 +0100

    Implementacion sin Intersection

--- Grafos/src/Vancouver/principal.java
+++ Grafos/src/Vancouver/principal.java
@@ -13,34 +13,7 @@ public class principal {
     }
 
     public static void CrearGrafo(File fichero, Graph<String, Segment> graph) {
-
-        FicheroSecuencial<Segment> fs = null;
-
-        try {
-            fs = new FicheroSecuencial<>(fichero.getPath(), ";");
-
-            while (!fs.isEndOfFile()) {
-
-                Segment seg = new Segment();
-                fs.read(seg);
-
-                String origin = seg.getOriginId();
-                String dest = seg.getDestinationId();
-
-                Vertex<String> vO = new Vertex<>(origin);
-                Vertex<String> vD = new Vertex<>(dest);
-
-                if (!graph.containsVertex(vO)) graph.insertVertex(vO);
-                if (!graph.containsVertex(vD)) graph.insertVertex(vD);
-
-                Edge<Segment> e = new Edge<>(vO, vD, seg);
-                graph.insertEdge(e);
-            }
-
-        } catch (Exception e) {
-            System.err.println("Error: " + e.getMessage());
-        } finally {
-            if (fs != null) fs.close();
-        }
+        
+        
     }
 }
